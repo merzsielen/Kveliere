05ac6c48a8f5b8af8ffdf766d1a523318981523a
Collin Brown <CJBrown7966@gmail.com>
2022-01-09 21:59:10 -0800

    Published Website

--- site.hs
+++ site.hs
@@ -3,9 +3,13 @@
 import           Data.Monoid (mappend)
 import           Hakyll
 import qualified GHC.IO.Encoding as E
+import           Data.List              (sortBy,isSuffixOf)
+import           System.FilePath.Posix  (takeBaseName,takeDirectory,(</>))
 
 
 --------------------------------------------------------------------------------
+
+
 main :: IO ()
 main = do
     E.setLocaleEncoding E.utf8
@@ -19,25 +23,37 @@ main = do
             route   idRoute
             compile copyFileCompiler
 
+        match "font/roboto-mono/*" $ do
+            route   idRoute
+            compile copyFileCompiler
+
+        match "font/tinos/*" $ do
+            route   idRoute
+            compile copyFileCompiler
+
         match "css/*" $ do
             route   idRoute
             compile compressCssCompiler
 
+        match "favicon.ico" $ do
+            route idRoute
+            compile copyFileCompiler 
+
         match (fromList ["about.markdown", "contact.markdown"]) $ do
-            route   $ setExtension "html"
+            route   $ cleanRoute
             compile $ pandocCompiler
                 >>= loadAndApplyTemplate "templates/default.html" defaultContext
                 >>= relativizeUrls
 
         match "posts/*" $ do
-            route $ setExtension "html"
+            route $ cleanRoute
             compile $ pandocCompiler
                 >>= loadAndApplyTemplate "templates/post.html"    postCtx
                 >>= loadAndApplyTemplate "templates/default.html" postCtx
                 >>= relativizeUrls
 
         create ["archive.html"] $ do
-            route idRoute
+            route cleanRoute
             compile $ do
                 posts <- recentFirst =<< loadAll "posts/*"
                 let archiveCtx =
@@ -72,3 +88,19 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+cleanRoute :: Routes 
+cleanRoute = customRoute createIndexRoute
+    where
+        createIndexRoute ident = takeDirectory p
+                                    </> takeBaseName p
+                                    </> "index.html"
+                                where p = toFilePath ident
+
+cleanIndexUrls :: Item String -> Compiler (Item String)
+cleanIndexUrls = return . fmap (withUrls clean)
+    where
+        idx = "index.html"
+        clean url
+            | idx `isSuffixOf` url = take (length url - length idx) url
+            | otherwise            = url
